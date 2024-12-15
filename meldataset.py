e0c9433b2343bfabcc21c6ce4f0cfb91b8b36f67
JIHYUN <84016081+YJH-jm@users.noreply.github.com>
2024-12-15 12:43:43 +0900

    Change seed number

--- meldataset.py
+++ meldataset.py
@@ -47,10 +47,10 @@ class TextCleaner:
                 print("000000000000000000000000000000000000000000000000000000000000000000000000000000", char)
         return indexes
 
-seed = 16
+# seed = 16
 # seed = 52
 # seed = 42
-# seed = 1
+seed = 1
 # seed = 1234
 # seed = 46
 np.random.seed(seed)
